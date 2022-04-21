ee002020823bb336d9805faf4b8094c16facda70
erwan.vivien <erwan.vivien@epita.fr>
2022-04-21 16:18:12 +0200

    fix: boundary check for negative frame

--- src/decoder.py
+++ src/decoder.py
@@ -6,6 +6,9 @@ import cv2
 def decode(buffer: BufferedReader, n=1):
     out = []
 
+    if n <= 0:
+        return []
+
     for _ in range(0, n):
         nb_bytes = buffer.read(8)
         nb_bytes = int.from_bytes(nb_bytes, byteorder="big")
