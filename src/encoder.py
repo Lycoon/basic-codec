19b963f55db63baeaa779a6df9ee5eca3d960754
erwan.vivien <erwan.vivien@epita.fr>
2022-04-21 16:05:37 +0200

    fix: quality is now 95

--- src/encoder.py
+++ src/encoder.py
@@ -6,7 +6,7 @@ import cv2
 def encode(frame: NDArray, buffer: BufferedWriter):
     # contents is the jpeg frame in a byte buffer
     is_success, contents = cv2.imencode(
-        ext=".jpg", img=frame, params=[cv2.IMWRITE_JPEG_QUALITY, 50]
+        ext=".jpg", img=frame, params=[cv2.IMWRITE_JPEG_QUALITY, 95]
     )
 
     if not is_success:
