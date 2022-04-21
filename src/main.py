098e938dbc51333105930e82b47b7dfca69e12c8
erwan.vivien <erwan.vivien@epita.fr>
2022-04-21 14:03:07 +0200

    feature: can convert jpeg into buffer

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
-import numpy
-from numpy.typing import NDArray
-import imutils
+import sys
 from imutils.video import FileVideoStream
+import cv2
 
 
 def main():
     # Main encoding loop while reading mp4
     vs = FileVideoStream("./Big_Buck_Bunny_1080_10s_10MB.mp4").start()
-    while not (frame := vs.read()) is None:
-        print(frame.shape)
-        frame = imutils.resize(frame, width=400)
+    i = 0
+    while (i := i + 1) and not (frame := vs.read()) is None:
+        # contents is the jpeg frame in a byte buffer
+        is_success, contents = cv2.imencode(ext=".jpg", img=frame)
+        if not is_success:
+            print(f"Could not read frame {i}", file=sys.stderr)
+            continue
 
 
 if __name__ == "__main__":
