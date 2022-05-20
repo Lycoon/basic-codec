33a683bae4d3f25cfd2822b8aa93e16e52c3516c
erwan.vivien <erwan.vivien@epita.fr>
2022-05-20 15:47:27 +0200

    v3: saves block at same place

--- src/encoder.py
+++ src/encoder.py
@@ -31,18 +31,27 @@ def abs_diff(A: NDArray, B: NDArray):
 BLOCK_SIZE = 8
 
 i = 0
+ALL_BLOCKS = np.zeros(shape=(1, 1))
 
 
 def encode(
     current_frame: NDArray, p_frame: NDArray, out: BufferedWriter, i=None, macroblock=16
 ) -> NDArray:
     BLOCK_SIZE = macroblock
+    global ALL_BLOCKS
+
     if p_frame is None:
         # Codec file Header
         initial_frame, initial_frame_size = jpeg(current_frame)
         write_buf_bytes(1, 1, out)  # flag
         write_buf_bytes(initial_frame_size, 4, out)
         write_buf(initial_frame, out)
+
+        dim = current_frame.shape[0] * current_frame.shape[1]
+        ALL_BLOCKS = np.zeros(
+            shape=(dim // BLOCK_SIZE**2, BLOCK_SIZE, BLOCK_SIZE, 3), dtype=np.uint8
+        )
+
         return current_frame
 
     r1 = range(0, current_frame.shape[0], BLOCK_SIZE)
@@ -56,22 +65,22 @@ def encode(
     # absolute difference
     diff_gray = abs_diff(block_current_gray, block_p_frame_gray) / 255
 
-    f = open("tmp.blocks", "wb")
-
     # Loops over all blocks
     RATIO = 1 / 5
     if not i is None and i % 10 == 0:
         RATIO = 1 / 6
 
+    block_idx = 0
     for ii, jj in itertools.product(r1, r2):
         block = diff_gray[ii : ii + BLOCK_SIZE, jj : jj + BLOCK_SIZE]
         sum_diff = np.sum(block)
         if sum_diff > BLOCK_SIZE**2 * RATIO:
             best_coords.append((ii, jj))
             block = current_frame[ii : ii + BLOCK_SIZE, jj : jj + BLOCK_SIZE]
-            np.save(f, block)
-
-    f.close()
+            ALL_BLOCKS[
+                block_idx, : block.shape[0], : block.shape[1], : block.shape[2]
+            ] = block
+            block_idx += 1
 
     rect = np.copy(p_frame)
     for ii, jj in best_coords:
@@ -79,13 +88,8 @@ def encode(
         write_buf_bytes(jj // BLOCK_SIZE, 2, out)
         block = current_frame[ii : ii + BLOCK_SIZE, jj : jj + BLOCK_SIZE]
         p_frame[ii : ii + BLOCK_SIZE, jj : jj + BLOCK_SIZE] = block
-        np.save(out, block)
-        cv2.rectangle(
-            rect,
-            (jj, ii),
-            (jj + BLOCK_SIZE, ii + BLOCK_SIZE),
-            [255, 0, 255],
-        )
+
+    np.save(out, ALL_BLOCKS[:block_idx])
 
     cv2.imwrite(f"out/p_frame_{i:0>5}.jpeg", p_frame)
     cv2.imwrite(f"out/p_frame_blocks_{i:0>5}.jpeg", rect)
